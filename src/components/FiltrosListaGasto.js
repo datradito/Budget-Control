74d347fafdd2a24aa45f564aa06527a7b61b11aa
Daniel Agustin Tradito <datradito@gmail.com>
2019-12-19 17:21:25 -0300

    fix server heroku

--- src/components/FiltrosListaGasto.js
+++ src/components/FiltrosListaGasto.js
@@ -74,7 +74,7 @@ const mapStateToProps = (state) => ({
     filters : state.filters
 });
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = (dispatch) => ({
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByFecha: () => dispatch(sortByFecha()),
     sortByImporte: () => dispatch(sortByImporte()),
